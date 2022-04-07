babdf15d399c74cec446665ef60273f10ef063a6
Jaque <alunowh2021@gmail.com>
2022-04-07 08:09:17 -0300

    app

---
+++ config.js
@@ -0,0 +1,15 @@
+import firebase from 'firebase';
+require("@firebase/firestore");
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB4d1XwfrH7xIa3Do3pPGK4RfFfvhEXtPc",
+  authDomain: "bibliotecaeletronica-44624.firebaseapp.com",
+  projectId: "bibliotecaeletronica-44624",
+  storageBucket: "bibliotecaeletronica-44624.appspot.com",
+  messagingSenderId: "743606311658",
+  appId: "1:743606311658:web:3f164e29f0e42531121593"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+export default firebase.firestore();
